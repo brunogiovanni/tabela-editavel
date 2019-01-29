764894f33c0d3c5f2386607cf1125dcfd1b0702a
Bruno Giovanni <brunobraganca@outlook.com>
2019-01-29 12:18:46 -0200

    Configuração e implementação do handsontable;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {HotTable} from '@handsontable/react';
+import 'handsontable/dist/handsontable.full.css';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+      colHeaders: ['Nome', 'Sexo', 'Planeta'],
+      settings: {
+        columns: [
+          {
+            data: "name", type: "text", title: 'Nome'
+          },
+          {
+            data: "gender", title: 'Sexo', editor: 'select', selectOptions: ['male', 'female']
+          },
+          {
+            data: "homeplanet", title: 'Planeta', type: "text"
+          }
+        ],
+        allowInsertRow: true
+      }
+    };
+
+    this._submeterDados = this._submeterDados.bind(this);
+
+    this._listarPessoas();
+  }
+
+  _listarPessoas() {
+    fetch('https://swapi.co/api/people/')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      if (responseJson.count > 0) {
+        for (let i in responseJson.results) {
+          this._pegarPlaneta(responseJson.results[i]);
+        }
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  _pegarPlaneta(pessoa) {
+    fetch(pessoa.homeworld)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      let dados = this.state.data;
+      if (responseJson !== null) {
+        dados.push({
+          name: pessoa.name, gender: pessoa.gender,
+          homeplanet: responseJson.name});
+      } else {
+        dados.push({
+          name: pessoa.name, gender: pessoa.gender,
+          homeplanet: 'unknow'});
+      }
+      this.setState({data: dados});
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  _submeterDados() {
+    fetch('url/da/sua/api', {
+      method: 'post',
+      body: JSON.stringify(this.state.data)
+    })
+    .then()
+    .catch();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <HotTable data={this.state.data} colHeaders={this.state.colHeaders} settings={this.state.settings} />
+        <button type="button" onClick={this._submeterDados}>Salvar</button>
       </div>
     );
   }
